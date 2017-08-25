82a3a46ad9a4044b19ac8db6b7462ad92217248d
Xcelerator1 <darkxcelerator@gmail.com>
2017-08-25 18:08:39 +0600

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,15 @@
+cd~/
+mkdir a
+cd a
+mkdir ~/bin
+PATH=~/bin:$PATH
+curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+chmod a+x ~/bin/repo
+yes | repo init -u https://github.com/Xcelerator1/patchrom -b marshmallow
+repo sync -c
+source build/envsetup.sh
+cv3750
+make fullota
+sudo apt install wput
+export DATEA=$(date +"%d%m%y")
+wput out/f*.zip ftp://Xcelerator:2SUKY5hT0vMq@uploads.androidfilehost.com:21/Miui8-v3750-$DATEA.
